895889cef52dbedde88e585a80c9bb4fc8b2e457
yusuf105 <39971480+yusuf105@users.noreply.github.com>
2021-12-15 20:22:57 +0700

    Add files via upload

---
+++ YMWA.js
@@ -0,0 +1,11 @@
+{
+	"version": "1.1",
+	"download": "https://ymmods-official.blogspot.com/2021/04/ymwa.html",
+	"update": [
+	
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/yusuf105/YM/main/changelog-en.html"
+		}
+	]
+}
