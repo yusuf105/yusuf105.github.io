5383af7c2c275767c6d5c0bf96cf2b1666ccdc14
yusuf105 <39971480+yusuf105@users.noreply.github.com>
2021-12-15 20:30:19 +0700

    Update UCWA.js

--- UCWA.js
+++ UCWA.js
@@ -3,8 +3,16 @@
 	"download": "https://ymmods-official.blogspot.com/2021/04/ymwa.html",
 	"update": [
 	
-		{
+			{
 			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/yusuf105/updater/main/changelog-en.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/yusuf105/updater/main/changelog-en.html"
+		},
+		{
+			"languaje": "pt",
 			"changelog": "https://raw.githubusercontent.com/yusuf105/updater/main/changelog-en.html"
 		}
 	]
