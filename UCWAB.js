eab1e0945953cde78a0f3d85121c4892fefc54cf
yusuf105 <39971480+yusuf105@users.noreply.github.com>
2022-01-27 13:34:11 +0700

    Create UCWAB.js

---
+++ UCWAB.js
@@ -0,0 +1,19 @@
+{
+	"version": "0.1",
+	"download": "https://uchimods-web.blogspot.com/2021/12/blog-post.html?m=1",
+	"update": [
+	
+			{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/yusuf105/updater/main/updaterbisnis.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/yusuf105/updater/main/updaterbisnis.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/yusuf105/updater/main/updaterbisnis.html"
+		}
+	]
+}
